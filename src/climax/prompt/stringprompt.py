3729bd891320ba7afc901607ddeeba39653eadf2
Snap <XxSnapperGeexX@gmail.com>
2026-08-31 14:25:49 -0700

    Add exec_string_loop method to StringPrompt class.

--- src/climax/prompt/stringprompt.py
+++ src/climax/prompt/stringprompt.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable
 from dataclasses import dataclass, field
+from typing import Final
 
 
 @dataclass(frozen=True)
@@ -9,3 +10,21 @@ class StringPrompt:
     invalid_string_message_generator: Callable[[str], str] | None
     formatter: Callable[[str], str] | None = field(kw_only=True, default=None)
     ps1: str | None = field(kw_only=True, default=None)
+
+    def exec_string_loop(self) -> str:
+        ps1: Final = self.ps1 if self.ps1 is not None else ""
+        _input = input(self.message + ps1)
+        formatted_input = self.formatter(_input) if self.formatter else _input
+
+        if self.string_validator:
+            while not self.string_validator(formatted_input):
+                invalid_input_string = (
+                    self.invalid_string_message_generator(_input)
+                    if self.invalid_string_message_generator
+                    else ""
+                )
+
+                _input = input(invalid_input_string + self.message + ps1)
+                formatted_input = self.formatter(_input) if self.formatter else _input
+
+        return formatted_input
