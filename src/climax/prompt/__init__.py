d4e11f15d65f7e575b127e79418e704e2f118b70
Snap <XxSnapperGeexX@gmail.com>
2026-08-31 17:32:55 -0700

    Add default validator and invalid message generator and exec_input_loop method to Prompt class.

--- src/climax/prompt/__init__.py
+++ src/climax/prompt/__init__.py
@@ -1,8 +1,17 @@
 from collections.abc import Callable
 from dataclasses import dataclass, field
-from typing import final
+from functools import cached_property
+from typing import Literal, final
 
-from .stringprompt import StringPrompt
+from .stringprompt import StringPrompt, _always_true_predicate
+
+
+def _identity_function[T](arg: T) -> T:
+    return arg
+
+
+def _default_invalid_value_message_generator(_: object) -> Literal[""]:
+    return ""
 
 
 @final
@@ -11,6 +20,29 @@ class Prompt[ValueType](StringPrompt):
     converter: Callable[[str], ValueType]
     validator: Callable[[ValueType], bool] | None
     invalid_value_message_generator: Callable[[ValueType], str] | None
-    normalizer: Callable[[ValueType], ValueType] | None = field(
-        kw_only=True, default=None
-    )
+
+    @cached_property
+    def _validator(self) -> Callable[[ValueType], bool]:
+        return self.validator if self.validator else _always_true_predicate
+
+    @cached_property
+    def _invalid_value_message_generator(self) -> Callable[[ValueType], str]:
+        return (
+            self.invalid_value_message_generator
+            if self.invalid_value_message_generator
+            else _default_invalid_value_message_generator
+        )
+
+    def exec_input_loop(self) -> ValueType:
+        string_input = super().exec_string_input_loop()
+        converted_input = self.converter(string_input)
+
+        while not self._validator(converted_input):
+            invalid_input_string = self._invalid_value_message_generator(
+                converted_input
+            )
+            print(invalid_input_string, end=None)
+            string_input = super().exec_string_input_loop()
+            converted_input = self.converter(string_input)
+
+        return converted_input
