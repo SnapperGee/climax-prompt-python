873ad7e4e873cdc40869e7467c25be37130e1c28
Snap <XxSnapperGeexX@gmail.com>
2026-09-01 00:48:15 -0700

    Remove default invalid value message generator from Prompt class.

--- src/climax/prompt/__init__.py
+++ src/climax/prompt/__init__.py
@@ -1,13 +1,9 @@
 from collections.abc import Callable
 from dataclasses import dataclass
 from functools import cached_property
-from typing import Literal, final
+from typing import final
 
 from .stringprompt import StringPrompt, _always_true_predicate
-
-
-def _default_invalid_value_message_generator(_: object) -> Literal[""]:
-    return ""
 
 
 @final
@@ -21,22 +17,14 @@ class Prompt[ValueType](StringPrompt):
     def _validator(self) -> Callable[[ValueType], bool]:
         return self.validator or _always_true_predicate
 
-    @cached_property
-    def _invalid_value_message_generator(self) -> Callable[[ValueType], str]:
-        return (
-            self.invalid_value_message_generator
-            or _default_invalid_value_message_generator
-        )
-
     def exec_input_loop(self) -> ValueType:
         string_input = super().exec_string_input_loop()
         converted_input = self.converter(string_input)
 
         while not self._validator(converted_input):
-            invalid_input_string = self._invalid_value_message_generator(
-                converted_input
-            )
-            print(invalid_input_string, end=None)
+            if self.invalid_value_message_generator:
+                print(self.invalid_value_message_generator(converted_input), end=None)
+
             string_input = super().exec_string_input_loop()
             converted_input = self.converter(string_input)
 
