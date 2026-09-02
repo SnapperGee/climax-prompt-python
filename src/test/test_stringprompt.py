6774c5f4b7daa9178cdb320aba88734d43fd0f14
Snap <XxSnapperGeexX@gmail.com>
2026-09-02 14:00:13 -0700

    Add shared consts for StringPrompt tests.

--- src/test/test_stringprompt.py
+++ src/test/test_stringprompt.py
@@ -1,46 +1,49 @@
 import builtins
 from collections.abc import Callable
-from typing import Final
+from typing import Final, Literal
 from unittest.mock import patch
 
 from climax.prompt import StringPrompt
 from pytest import mark
 
+_MESSAGE: Final = "message\n"
+
+_PS1: Final = ">>> "
+
+
+def _always_true_string_predicate(_: str) -> Literal[True]:
+    return True
+
+
+def _invalid_string_message_generator(raw_input: str, formatted_input: str) -> str:
+    return f'Invalid raw input: "{raw_input}". Invalid formatted input: "{formatted_input}"\n"'
+
 
 def test_stringprompt_fields() -> None:
-    message: Final = "Input a palindrome.\n"
-
-    def string_validator(string: str) -> bool:
-        return string == string[::-1]
-
-    def invalid_string_message_generator(raw_string: str, _: str) -> str:
-        return f'"{raw_string}" is not a palindrome.\n'
 
     def formatter(string: str) -> str:
         return string.strip()
 
-    ps1: Final = ">>> "
-
     string_prompt: Final = StringPrompt(
-        message,
-        string_validator,
-        invalid_string_message_generator,
+        _MESSAGE,
+        _always_true_string_predicate,
+        _invalid_string_message_generator,
         formatter=formatter,
-        ps1=ps1,
+        ps1=_PS1,
     )
 
-    assert string_prompt.message is message
-    assert string_prompt.string_validator is string_validator
-    assert string_prompt.invalid_string_message_generator is invalid_string_message_generator
+    assert string_prompt.message is _MESSAGE
+    assert string_prompt.string_validator is _always_true_string_predicate
+    assert string_prompt.invalid_string_message_generator is _invalid_string_message_generator
     assert string_prompt.formatter is formatter
-    assert string_prompt.ps1 is ps1
+    assert string_prompt.ps1 is _PS1
 
 
 def test_stringprompt_default_field_values() -> None:
     string_prompt: Final = StringPrompt(
-        "",
-        lambda _: True,
-        lambda _, __: "",
+        _MESSAGE,
+        _always_true_string_predicate,
+        _invalid_string_message_generator,
     )
 
     assert string_prompt.formatter is None
@@ -51,9 +54,9 @@ def test_stringprompt_default_format_method_is_identity_function() -> None:
     string: Final = "snake"
 
     string_prompt: Final = StringPrompt(
-        "",
-        lambda _: True,
-        lambda _, __: "",
+        _MESSAGE,
+        _always_true_string_predicate,
+        _invalid_string_message_generator,
     )
 
     assert string_prompt.format(string) is string
@@ -68,19 +71,14 @@ def test_stringprompt_default_format_method_is_identity_function() -> None:
     ),
 )
 def test_stringprompt_execStringInputLoop_with_valid_input(validator: Callable[[str], bool], user_input: str) -> None:
-    message: Final = "message"
-
-    def invalid_message_generator(raw_input: str, formatted_input: str) -> str:
-        return f'Invalid raw input: "{raw_input}". Invalid formatted input: "{formatted_input}"\n"'
-
     string_prompt: Final = StringPrompt(
-        message,
+        _MESSAGE,
         validator,
-        invalid_message_generator,
+        _invalid_string_message_generator,
     )
 
     with patch.object(builtins, "input", side_effect=(user_input,)) as mock_input:
         result: Final = string_prompt.exec_string_input_loop()
 
     assert result == user_input
-    mock_input.assert_called_once_with(message)
+    mock_input.assert_called_once_with(_MESSAGE)
