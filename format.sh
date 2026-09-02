7aecae41b772b99091354c54ea6a04665bbcfe7e
Snap <XxSnapperGeexX@gmail.com>
2026-09-02 12:37:40 -0700

    Add import sorting fix to format script.

--- format.sh
+++ format.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 printf 'Formatting src directory source code with ruff...\n'
-poetry run ruff format ./src
+poetry run ruff format ./src && poetry run ruff check --select I --fix ./src
