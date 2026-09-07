c67414953020dd28d8344d568f91342f0f179c91
Snap <XxSnapperGeexX@gmail.com>
2026-09-07 15:52:03 -0700

    Update tests to reflect validators generating error messages.

--- src/test/test_stringprompt.py
+++ src/test/test_stringprompt.py
@@ -1,6 +1,6 @@
 import builtins
 from collections.abc import Callable
-from typing import Final, Literal
+from typing import Final
 from unittest.mock import call, patch
 
 from climax.prompt import StringInputLoopResult, StringPrompt
@@ -11,39 +11,37 @@ _MESSAGE: Final = "message\n"
 _PS1: Final = ">>> "
 
 
-def _always_true_string_predicate(_: str) -> Literal[True]:
-    return True
+def _always_none_returning_function(_: str) -> None:
+    return None
 
 
-def _invalid_string_message_generator(raw_input: str, formatted_input: str) -> str:
-    return f'Invalid raw input: "{raw_input}". Invalid formatted input: "{formatted_input}"\n"'
+def _string_is_empty(string: str) -> str | None:
+    return None if len(string) == 0 else f'String is not empty: "{string}"'
 
 
-def _string_is_empty(string: str) -> bool:
-    return len(string) == 0
+def _string_is_not_empty(string: str) -> str | None:
+    return None if len(string) != 0 else "String is empty"
 
 
-def _string_is_not_empty(string: str) -> bool:
-    return len(string) != 0
+def _string_is_palindrome(string: str) -> str | None:
+    return None if string == string[::-1] else f'String is not a palindrome: "{string}"'
 
 
-def _string_is_palindrome(string: str) -> bool:
-    return string == string[::-1]
+def _string_is_digit(string: str) -> str | None:
+    return None if string.isdigit() else f'String is not a digit: "{string}"'
 
 
 def test_stringprompt_fields() -> None:
 
     string_prompt: Final = StringPrompt(
         _MESSAGE,
-        _always_true_string_predicate,
-        _invalid_string_message_generator,
+        _always_none_returning_function,
         formatter=str.strip,
         ps1=_PS1,
     )
 
     assert string_prompt.message is _MESSAGE
-    assert string_prompt.string_validator is _always_true_string_predicate
-    assert string_prompt.invalid_string_message_generator is _invalid_string_message_generator
+    assert string_prompt.string_validator is _always_none_returning_function
     assert string_prompt.formatter is str.strip
     assert string_prompt.ps1 is _PS1
 
@@ -51,8 +49,7 @@ def test_stringprompt_fields() -> None:
 def test_stringprompt_default_field_values() -> None:
     string_prompt: Final = StringPrompt(
         _MESSAGE,
-        _always_true_string_predicate,
-        _invalid_string_message_generator,
+        _always_none_returning_function,
     )
 
     assert string_prompt.formatter is None
@@ -64,8 +61,7 @@ def test_stringprompt_default_format_method_is_identity_function() -> None:
 
     string_prompt: Final = StringPrompt(
         _MESSAGE,
-        _always_true_string_predicate,
-        _invalid_string_message_generator,
+        _always_none_returning_function,
     )
 
     assert string_prompt.format(string) is string
@@ -75,25 +71,23 @@ def test_stringprompt_default_format_method_is_identity_function() -> None:
     "validator,user_input,formatter,ps1",
     (
         (_string_is_empty, "", None, None),
-        (str.isdigit, "123", None, None),
+        (_string_is_digit, "123", None, None),
         (_string_is_palindrome, "level", None, None),
         (_string_is_empty, "         ", str.strip, None),
-        (str.isdigit, "     123     ", str.strip, None),
+        (_string_is_digit, "     123     ", str.strip, None),
         (_string_is_palindrome, "level         ", str.strip, None),
         (_string_is_empty, "", None, _PS1),
-        (str.isdigit, "123", None, _PS1),
+        (_string_is_digit, "123", None, _PS1),
         (_string_is_palindrome, "level", None, _PS1),
         (_string_is_empty, "         ", str.strip, _PS1),
-        (str.isdigit, "     123     ", str.strip, _PS1),
+        (_string_is_digit, "     123     ", str.strip, _PS1),
         (_string_is_palindrome, "level         ", str.strip, _PS1),
     ),
 )
 def test_stringprompt_execStringInputLoop_with_valid_input_and_no_raw_string_return_argument(
-    validator: Callable[[str], bool], user_input: str, formatter: Callable[[str], str] | None, ps1: str | None
+    validator: Callable[[str], str | None], user_input: str, formatter: Callable[[str], str] | None, ps1: str | None
 ) -> None:
-    string_prompt: Final = StringPrompt(
-        _MESSAGE, validator, _invalid_string_message_generator, formatter=formatter, ps1=ps1
-    )
+    string_prompt: Final = StringPrompt(_MESSAGE, validator, formatter=formatter, ps1=ps1)
 
     with patch.object(builtins, "input", side_effect=(user_input,)) as mock_input:
         result: Final = string_prompt.exec_string_input_loop()
@@ -106,25 +100,23 @@ def test_stringprompt_execStringInputLoop_with_valid_input_and_no_raw_string_ret
     "validator,user_input,formatter,ps1",
     (
         (_string_is_empty, "", None, None),
-        (str.isdigit, "123", None, None),
+        (_string_is_digit, "123", None, None),
         (_string_is_palindrome, "level", None, None),
         (_string_is_empty, "         ", str.strip, None),
-        (str.isdigit, "     123     ", str.strip, None),
+        (_string_is_digit, "     123     ", str.strip, None),
         (_string_is_palindrome, "level         ", str.strip, None),
         (_string_is_empty, "", None, _PS1),
-        (str.isdigit, "123", None, _PS1),
+        (_string_is_digit, "123", None, _PS1),
         (_string_is_palindrome, "level", None, _PS1),
         (_string_is_empty, "         ", str.strip, _PS1),
-        (str.isdigit, "     123     ", str.strip, _PS1),
+        (_string_is_digit, "     123     ", str.strip, _PS1),
         (_string_is_palindrome, "level         ", str.strip, _PS1),
     ),
 )
 def test_stringprompt_execStringInputLoop_with_valid_input_and_false_raw_string_return_argument(
-    validator: Callable[[str], bool], user_input: str, formatter: Callable[[str], str] | None, ps1: str | None
+    validator: Callable[[str], str | None], user_input: str, formatter: Callable[[str], str] | None, ps1: str | None
 ) -> None:
-    string_prompt: Final = StringPrompt(
-        _MESSAGE, validator, _invalid_string_message_generator, formatter=formatter, ps1=ps1
-    )
+    string_prompt: Final = StringPrompt(_MESSAGE, validator, formatter=formatter, ps1=ps1)
 
     with patch.object(builtins, "input", side_effect=(user_input,)) as mock_input:
         result: Final = string_prompt.exec_string_input_loop(False)
@@ -137,25 +129,23 @@ def test_stringprompt_execStringInputLoop_with_valid_input_and_false_raw_string_
     "validator,user_input,formatter,ps1",
     (
         (_string_is_empty, "", None, None),
-        (str.isdigit, "123", None, None),
+        (_string_is_digit, "123", None, None),
         (_string_is_palindrome, "level", None, None),
         (_string_is_empty, "         ", str.strip, None),
-        (str.isdigit, "     123     ", str.strip, None),
+        (_string_is_digit, "     123     ", str.strip, None),
         (_string_is_palindrome, "level         ", str.strip, None),
         (_string_is_empty, "", None, _PS1),
-        (str.isdigit, "123", None, _PS1),
+        (_string_is_digit, "123", None, _PS1),
         (_string_is_palindrome, "level", None, _PS1),
         (_string_is_empty, "         ", str.strip, _PS1),
-        (str.isdigit, "     123     ", str.strip, _PS1),
+        (_string_is_digit, "     123     ", str.strip, _PS1),
         (_string_is_palindrome, "level         ", str.strip, _PS1),
     ),
 )
 def test_stringprompt_execStringInputLoop_with_valid_input_and_raw_string_return_argument(
-    validator: Callable[[str], bool], user_input: str, formatter: Callable[[str], str] | None, ps1: str | None
+    validator: Callable[[str], str | None], user_input: str, formatter: Callable[[str], str] | None, ps1: str | None
 ) -> None:
-    string_prompt: Final = StringPrompt(
-        _MESSAGE, validator, _invalid_string_message_generator, formatter=formatter, ps1=ps1
-    )
+    string_prompt: Final = StringPrompt(_MESSAGE, validator, formatter=formatter, ps1=ps1)
 
     with patch.object(builtins, "input", side_effect=(user_input,)) as mock_input:
         result: Final = string_prompt.exec_string_input_loop(True)
@@ -171,37 +161,28 @@ def test_stringprompt_execStringInputLoop_with_valid_input_and_raw_string_return
     "validator,user_input,formatter,ps1",
     (
         (_string_is_not_empty, "", None, None),
-        (str.isdigit, "A123", None, None),
+        (_string_is_digit, "A123", None, None),
         (_string_is_palindrome, "Knights who say ni", None, None),
         (_string_is_not_empty, "         ", str.strip, None),
-        (str.isdigit, "  A   123     ", str.strip, None),
+        (_string_is_digit, "  A   123     ", str.strip, None),
         (_string_is_palindrome, "Knights who say ni", str.strip, None),
         (_string_is_not_empty, "", None, _PS1),
-        (str.isdigit, "A123", None, _PS1),
+        (_string_is_digit, "A123", None, _PS1),
         (_string_is_palindrome, "Knights who say ni", None, _PS1),
         (_string_is_not_empty, "         ", str.strip, _PS1),
-        (str.isdigit, "  A   123     ", str.strip, _PS1),
+        (_string_is_digit, "  A   123     ", str.strip, _PS1),
         (_string_is_palindrome, "Knights who say ni", str.strip, _PS1),
     ),
 )
 def test_stringprompt_execStringInputLoop_with_invalid_input(
-    validator: Callable[[str], bool], user_input: str, formatter: Callable[[str], str] | None, ps1: str | None
+    validator: Callable[[str], str | None], user_input: str, formatter: Callable[[str], str] | None, ps1: str | None
 ) -> None:
-    string_prompt: Final = StringPrompt(
-        _MESSAGE, validator, _invalid_string_message_generator, formatter=formatter, ps1=ps1
-    )
+    string_prompt: Final = StringPrompt(_MESSAGE, validator, formatter=formatter, ps1=ps1)
 
     with patch.object(builtins, "input", side_effect=(user_input,)) as mock_input, raises(StopIteration):
         string_prompt.exec_string_input_loop()
 
     assert mock_input.call_args_list == [
         call(_MESSAGE + (ps1 or "")),
-        call(
-            _invalid_string_message_generator(
-                user_input,
-                formatter(user_input) if formatter else user_input,
-            )
-            + _MESSAGE
-            + (ps1 or "")
-        ),
+        call((validator(formatter(user_input) if formatter else user_input) or "") + _MESSAGE + (ps1 or "")),
     ]
