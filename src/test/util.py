e79dadc45f2eb4bdaa44d64a471c43e5f6a9cfb0
Snap <XxSnapperGeexX@gmail.com>
2026-09-09 10:40:06 -0700

    Use isdecimal instead of is digit in test validator function.

--- src/test/util.py
+++ src/test/util.py
@@ -6,4 +6,4 @@ PS1: Final = ">>> "
 
 
 def string_is_digit(strings: tuple[str, str]) -> str | None:
-    return None if strings[0].isdigit() else f'String is not a digit: "{strings[1]}".\n'
+    return None if strings[0].isdecimal() else f'String is not a digit: "{strings[1]}".\n'
