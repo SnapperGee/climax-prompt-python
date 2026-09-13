82d7b4dcbdca1ebec357ff155794a2853f68a25d
Snap <XxSnapperGeexX@gmail.com>
2026-09-13 09:16:59 -0700

    Revert "Add always true returning method to _util module."
    This reverts commit b7899896933c8c873f137f6f7c27f1f5f7f37149.

--- src/main/climax/prompt/_util.py
+++ src/main/climax/prompt/_util.py
@@ -1,6 +1,3 @@
-from typing import Literal
-
-
 def string_identity_function(string: str) -> str:
     r"""An identity function that consumes a ``string`` and returns the same ``string``."""
     return string
@@ -9,7 +6,3 @@ def string_identity_function(string: str) -> str:
 def always_none_returning_function(_: object) -> None:
     r"""A function that consumes an ``object`` and returns ``None``."""
     return
-
-
-def always_true_returning_function(_: object) -> Literal[True]:
-    return True
