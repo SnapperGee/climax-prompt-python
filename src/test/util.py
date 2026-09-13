348e20a802bbe40011c2cf186523044ad04bfb15
Snap <XxSnapperGeexX@gmail.com>
2026-09-13 09:17:30 -0700

    Add always true returning method to test util module.

--- src/test/util.py
+++ src/test/util.py
@@ -1,4 +1,4 @@
-from typing import Final
+from typing import Final, Literal
 
 MESSAGE: Final = "message\n"
 
@@ -11,3 +11,6 @@ def string_is_digit(strings: tuple[str, str]) -> str | None:
         if (strings[0].isdecimal() if not strings[0].startswith("-") else strings[0][1:].isdecimal())
         else f'String is not a digit: "{strings[1]}".\n'
     )
+
+def always_true_returning_function(_: object) -> Literal[True]:
+    return True
