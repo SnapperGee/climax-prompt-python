cf944756ea0f18d6f517b3a3246d06c88e24bf00
Snap <XxSnapperGeexX@gmail.com>
2026-09-13 08:36:53 -0700

    Remove create class method from PromptResult and make value field non optional.

--- src/main/climax/prompt/prompt_result.py
+++ src/main/climax/prompt/prompt_result.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
-from typing import Self, final
+from typing import final
 
 
 @final
 @dataclass(frozen=True)
 class PromptResult[ValueType]:
     original_input_string: str
-    value: ValueType | None = None
+    value: ValueType | None
     conversion_exception: Exception | None = None
 
     def __post_init__(self) -> None:
@@ -15,11 +15,3 @@ class PromptResult[ValueType]:
                 f"{type(self).__name__}: truthy `conversion_exception` with "
                 f"non-None `value`:\n{self.value=}\n\n{self.conversion_exception!r}"
             )
-
-    @classmethod
-    def create(cls, original_input_string: str, value_or_conversion_exception: ValueType | Exception) -> Self:
-        return (
-            cls(original_input_string, None, value_or_conversion_exception)
-            if isinstance(value_or_conversion_exception, Exception)
-            else cls(original_input_string, value_or_conversion_exception)
-        )
