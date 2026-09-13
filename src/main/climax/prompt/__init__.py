46e39614f4a4915d7a4dcd1f5b3ace3baec12289
Snap <XxSnapperGeexX@gmail.com>
2026-09-13 08:48:44 -0700

    Export PromptResult from prompt package.

--- src/main/climax/prompt/__init__.py
+++ src/main/climax/prompt/__init__.py
@@ -1,6 +1,7 @@
 from .prompt import Prompt
+from .prompt_result import PromptResult
 from .string_input import StringInput
 from .string_prompt import StringPrompt
 from .validator import StringValidator, Validator
 
-__all__ = ("Prompt", "StringInput", "StringPrompt", "StringValidator", "Validator")
+__all__ = ("Prompt", "PromptResult", "StringInput", "StringPrompt", "StringValidator", "Validator")
